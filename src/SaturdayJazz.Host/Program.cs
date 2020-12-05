59a181303b981bfdb52b6c13f8c4666f7fec1e3d
Paula Onea <paula.onea@gmail.com>
2020-12-05 15:39:36 +0000

    first commit

---
+++ src/SaturdayJazz.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaturdayJazz.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
